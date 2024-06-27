4ee2222ddb42e6d49a54db4e706353342d20c088
Gift-py <giftabumere247@gmail.com>
2024-06-27 11:11:36 +0100

    sigh how do people like this stuff? FE <<<

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
 const { exec } = require('child_process');
 
@@ -7,101 +7,85 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-let popup;
+let mainWindow;
+let persistentWindow;
+let highlight_checker_process;
 
-//function for creating popup window after highlighting has happened
-function createPopupWindow(x, y, word) {
-  if (popup) {
-    popup.close();
-  }
-
-  popup = new BrowserWindow({
-    width: 200,
-    height: 100,
-    frame: false,
-    alwaysOnTop: true,
-    transparent: true,
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width:800,
+    height:600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
 
-  popup.loadURL(`data:text/html,
-    <html>
-      <body style="margin:0; display: flex; align-items: center; justify-content: center;">
-        <div style="background: white; padding: 10px; border-radius: 5px;">
-          ${word}
-        </div>
-      </body>
-    </html>`);
-
-  popup.setPosition(x, y);
-  popup.show();
+  mainWindow.loadFile(path.join(__dirname, 'index.html'))
 }
 
+function createPersistentWindow() {
+  persistentWindow = new BrowserWindow({
+    width: 200,
+    height: 125,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  persistentWindow.loadFile(path.join(__dirname, 'windows', 'persistent.html'));
+}
+
+
 //function for checking when highlight has happened
-function monitorClipboard() {
+function highlightChecker() {
   // run bash script 
-  const highlight_checker = path.join(__dirname, 'scripts/highlight-check.sh')
-  const child = exec(`bash ${highlight_checker}`);
+  const highlight_checker_path = path.join(__dirname, 'scripts/highlight-check.sh')
+  highlight_checker_process = exec(`bash ${highlight_checker_path}`);
 
   // monitor bash script outputs
-  child.stdout.on('data', (data) => {
+  highlight_checker_process.stdout.on('data', (data) => {
     const output = data.trim().split(' @huh@ ');
     console.log(output)
     word = output[0]
-    const coords = output[1];
-    const x = parseInt(coords.split('x:')[1].split(' ')[0], 10);
-    const y = parseInt(coords.split('y:')[1], 10);
-    
-    createPopupWindow(x, y, word)
   });
 
-  child.stderr.on('data', (data) => {
+  highlight_checker_process.stderr.on('data', (data) => {
     console.error(`stderr: ${data}`);
   });
   
-  child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+  highlight_checker_process.on('close', (code) => {
+    console.log(`highlight_checker_process exited with code ${code}`);
   });
 }
-
-
-
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
-
-};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  //createWindow();
-  monitorClipboard();
+  
+  createMainWindow()
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  // app.on('activate', () => {
-  //   if (BrowserWindow.getAllWindows().length === 0) {
-  //     createWindow();
-  //   }
-  // });
 });
+
+ipcMain.on('toggle-highlight', (event, arg) => {
+  if (arg == 'on') {
+    createPersistentWindow()
+    highlightChecker()
+  } else if (arg == 'off') {
+    if (!persistentWindow.isDestroyed()) {
+      persistentWindow.close();
+    }
+    if (highlight_checker_process) {
+      highlight_checker_process.kill();
+    }
+  }
+});
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -112,5 +96,11 @@ app.on('window-all-closed', () => {
   }
 });
 
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
--- src/scripts/highlight-check.sh
+++ src/scripts/highlight-check.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 LAST_SELECTION=$(xclip -o -selection primary)
-LAST_COORDS=$(xdotool getmouselocation)
 
 while true; do
 	CURRENT_SELECTION=$(xclip -o -selection primary)
-	CURRENT_COORDS=$(xdotool getmouselocation)
 	if [ "$CURRENT_SELECTION" != "$LAST_SELECTION" ]; then
-		echo "$CURRENT_SELECTION @huh@ $CURRENT_COORDS"
+		echo "$CURRENT_SELECTION @huh@"
 		LAST_SELECTION=$CURRENT_SELECTION
-		LAST_COORDS=$CURRENT_COORDS
 	fi
 	sleep 1
 done
